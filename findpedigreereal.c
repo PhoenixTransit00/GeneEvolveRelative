bb1fca6a47865cbae9c6c2a42d54a51866ac06f2
emmanuelsapin <emmanuelsapin@hotmail.com>
2018-11-21 15:08:07 -0700

    Update findpedigreereal.c

--- findpedigreereal.c
+++ findpedigreereal.c
@@ -4,19 +4,22 @@
 *(The speed of this program can be greatly decreased by writing on file a big bunch of data from one command instead of how it is done here)
 *                                 
 ****************************************************************************************************************************************************/
-
+#include <time.h>
 #include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
 
-#define INT int32_t
+#include "readinteger.h"
+
 #define MAXINT INT32_MAX
 #define MAXPOP 1000000
 #define MAXGEN 1000
+#define INT int32_t
 #define MAXDEGREE 10
 #define pow(two,MAXDEGREE) 1024
+
 
 typedef struct  
 {	INT mum;
@@ -24,10 +27,24 @@ typedef struct
 } structsomeone;
 
 struct structrelative
-{	INT listrelative[pow(two,MAXDEGREE)];
-	INT numrelative;
-	INT highestgen; 
+{	//INT listrelative[pow(two,MAXDEGREE)];
+	INT numrelative[MAXPOP];
+	INT highestgen[MAXPOP];
 };
+
+INT getmaxpeople(char path[], INT generation);
+INT readinteger(FILE * file);
+INT readfile(char path[],INT generation,structsomeone people[],INT maxpeople[]);
+INT addtorelative(INT ancestor,INT indiv2,struct structrelative *ptrelative,INT gen,INT maxpeople[]);
+INT compare(INT indiv1,INT indiv2,INT indiv2origin,INT mingeneration,INT currentgeneration,struct structrelative *ptrelative,INT maxpeople[]);
+INT comparegenaway(INT indiv1,INT indiv2,INT indiv2origin,INT mingeneration,INT currentgeneration,struct structrelative *ptrelative,INT maxpeople[]);
+INT getgeneration(INT ID,INT maxpeople[]);
+INT printrelative(INT indiv1,INT indiv2,struct structrelative *ptrelative,INT maxpeople[],INT lastgeneration);
+INT initsave(char path[],INT numgen);
+INT saveallrelative(char path[],INT indiv1,INT lastgeneration,struct structrelative *ptrelative,INT maxpeople[],INT numgen,INT numgenpeople);
+
+
+
 //
 //     get the number of people or generation generation of the output of GeneEvolve located in the path path 
 //	
@@ -58,19 +75,7 @@ INT getmaxpeople(char path[], INT generation)
     fclose(fp); 
 	return (count-1);	
 }
-//
-//  read an positive integer from the file file
-//	
-INT readinteger(FILE * file)
-{	INT ID=0;
-	char carac;
-	do 
-	{	carac=getc(file);
-		if (carac!=EOF && carac>47 && carac<58) ID=ID*10+carac-48;
-	}	while (carac!=EOF && carac>47 && carac<58);
-	if (carac==EOF) ID=MAXINT;
-	return(ID); 	
-}
+
 //
 //  Read the file of generation generation of the output of GeneEvolve located in the path path to fill poeple with the ID of each individual, the ID of their mum
 //  then the ID of their dad
@@ -87,7 +92,7 @@ INT readfile(char path[],INT generation,structsomeone people[],INT maxpeople[])
 	strcat(filename,".txt");
     if ((file = fopen(filename, "r")) == NULL) return (1);
     printf("file %s opened\n",filename);
-	char carac;
+	char carac; 
     do {carac=getc(file);} while (carac!='\n' && carac!= EOF);
 	INT ID=0;
 	do 
@@ -116,11 +121,16 @@ INT readfile(char path[],INT generation,structsomeone people[],INT maxpeople[])
 //
 //  add ancestor to the relative
 //		
-INT addtorelative(INT ancestor,struct structrelative *ptrelative,INT gen,INT maxpeople[])
-{	ptrelative->listrelative[ptrelative->numrelative]=ancestor;
-	ptrelative->numrelative++;
-	if (ptrelative->highestgen<gen) ptrelative->highestgen=gen;
-	//printf("ID %d is a relative at generation %d\n",ancestor+1-maxpeople[gen],gen); 
+INT addtorelative(INT ancestor,INT indiv2,struct structrelative *ptrelative,INT gen,INT maxpeople[])
+{	//ptrelative->listrelative[ptrelative->numrelative[indiv2]]=ancestor;
+	ptrelative->numrelative[indiv2]++;
+	if (ptrelative->highestgen[indiv2]<gen) ptrelative->highestgen[indiv2]=gen;
+	/*printf("ID %d is a relative at generation %d, there are %d ancestor with individual %d, highest degree %d\n",
+			ancestor+1-maxpeople[gen],
+			gen,
+			ptrelative->numrelative[indiv2],
+			indiv2,
+			ptrelative->highestgen[indiv2]); */
 }
 //
 //  store each individual, the ID of their mum then the ID of their dad
@@ -129,41 +139,60 @@ structsomeone people[MAXPOP];
 //
 //  Search for relative of indiv1 and indiv2
 //		
-INT compare(INT indiv1,INT indiv2,INT mingeneration,INT currentgeneration,struct structrelative *ptrelative,INT maxpeople[])
+INT compare(INT indiv1,INT indiv2,INT indiv2origin,INT mingeneration,INT currentgeneration,struct structrelative *ptrelative,INT maxpeople[])
 {	//printf(" %p \n", ptrelative);
 	if (people[indiv1].mum==people[indiv2].mum)
-	{	addtorelative(people[indiv1].mum,ptrelative,currentgeneration,maxpeople);
+	{	addtorelative(people[indiv1].mum,indiv2origin,ptrelative,currentgeneration,maxpeople);
 		if (people[indiv1].dad==people[indiv2].dad)
-		{	addtorelative(people[indiv1].dad,ptrelative,currentgeneration,maxpeople);
+		{	addtorelative(people[indiv1].dad,indiv2origin,ptrelative,currentgeneration,maxpeople);
 			if (mingeneration<currentgeneration) 
-			{	compare(people[indiv1].mum,people[indiv1].dad,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+			{	compare(people[indiv1].mum,people[indiv1].dad,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
 			};
 		} else 
 		{ 	if (mingeneration<currentgeneration) 
-			{	compare(people[indiv1].dad,people[indiv2].dad,mingeneration,currentgeneration-1,ptrelative,maxpeople);
-				compare(people[indiv1].dad,people[indiv2].mum,mingeneration,currentgeneration-1,ptrelative,maxpeople);
-				compare(people[indiv1].mum,people[indiv2].dad,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+			{	compare(people[indiv1].dad,people[indiv2].dad,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+				compare(people[indiv1].dad,people[indiv2].mum,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+				compare(people[indiv1].mum,people[indiv2].dad,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
 			};
 		};
 	} else
 	{ 	if (people[indiv1].dad==people[indiv2].dad)
-		{	addtorelative(people[indiv1].dad,ptrelative,currentgeneration,maxpeople);
+		{	addtorelative(people[indiv1].dad,indiv2origin,ptrelative,currentgeneration,maxpeople);
 			if (mingeneration<currentgeneration) 
-			{	compare(people[indiv1].mum,people[indiv2].mum,mingeneration,currentgeneration-1,ptrelative,maxpeople);
-				compare(people[indiv1].mum,people[indiv2].dad,mingeneration,currentgeneration-1,ptrelative,maxpeople);
-				compare(people[indiv1].dad,people[indiv2].mum,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+			{	compare(people[indiv1].mum,people[indiv2].mum,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+				compare(people[indiv1].mum,people[indiv2].dad,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+				compare(people[indiv1].dad,people[indiv2].mum,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
 			};
 		} else 
 		{ 	if (mingeneration<currentgeneration) 
-			{	compare(people[indiv1].dad,people[indiv2].dad,mingeneration,currentgeneration-1,ptrelative,maxpeople);
-				compare(people[indiv1].mum,people[indiv2].dad,mingeneration,currentgeneration-1,ptrelative,maxpeople);
-				compare(people[indiv1].dad,people[indiv2].mum,mingeneration,currentgeneration-1,ptrelative,maxpeople);
-				compare(people[indiv1].mum,people[indiv2].mum,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+			{	compare(people[indiv1].dad,people[indiv2].dad,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+				compare(people[indiv1].mum,people[indiv2].dad,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+				compare(people[indiv1].dad,people[indiv2].mum,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+				compare(people[indiv1].mum,people[indiv2].mum,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
 			};
 		};
 	};
 	return (0);
 }
+//
+//  Search for relative of indiv1 and indiv2 being with one generation away
+//	
+INT comparegenaway(INT indiv1,INT indiv2,INT indiv2origin,INT mingeneration,INT currentgeneration,struct structrelative *ptrelative,INT maxpeople[])
+{	//printf(" %p \n", ptrelative);
+	if (people[indiv1].mum==indiv2)
+	{	addtorelative(indiv2,indiv2origin,ptrelative,currentgeneration,maxpeople);
+		compare(people[indiv1].dad,indiv2,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+	} else 
+	{	if (people[indiv1].dad==indiv2)
+		{	addtorelative(indiv2,indiv2origin,ptrelative,currentgeneration,maxpeople);
+			compare(people[indiv1].mum,indiv2,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+		} else 
+		{	compare(people[indiv1].dad,indiv2,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);
+			compare(people[indiv1].mum,indiv2,indiv2origin,mingeneration,currentgeneration-1,ptrelative,maxpeople);	
+		};
+	};
+	return (0);
+};
 //
 //  Get the generation of an ID from people
 //		
@@ -180,25 +209,29 @@ INT getgeneration(INT ID,INT maxpeople[])
 //		
 INT printrelative(INT indiv1,INT indiv2,struct structrelative *ptrelative,INT maxpeople[],INT lastgeneration)
 {	INT relat;
-	printf("There is/are %d relatives\n",ptrelative->numrelative );
-	if (ptrelative->numrelative>0) for(relat=0;relat<ptrelative->numrelative;relat++)
-	{	INT gen=getgeneration(ptrelative->listrelative[relat],maxpeople);
-		INT ID=ptrelative->listrelative[relat]+1-maxpeople[gen]; 
-		printf("ID %d is a relative at generation %d\n",ID,gen);
+	printf("There is/are %d relatives\n",ptrelative->numrelative[indiv2] );
+	if (ptrelative->numrelative[indiv2]>0) for(relat=0;relat<ptrelative->numrelative[indiv2];relat++)
+	{	//INT gen=getgeneration(ptrelative->listrelative[relat],maxpeople);
+		//INT ID=ptrelative->listrelative[relat]+1-maxpeople[gen]; 
+		//printf("ID %d is a relative at generation %d\n",ID,gen);
 	};
-	if (ptrelative->numrelative>0) printf("The most recent relative is at a degree of %d\n",lastgeneration - ptrelative->highestgen);
+	if (ptrelative->numrelative[indiv2]>0) printf("The most recent relative is at a degree of %d\n",lastgeneration - ptrelative->highestgen[indiv2]);
 	return (0);
 }
 //
 //  Create the file relative.txt
 //		
-INT initsave(char path[])
+INT initsave(char path[],INT numgen)
 {	FILE * file;
 	char filename[100]; 
 	strcpy (filename,path);
-	strcat(filename,".relatives.txt");
+	strcat(filename,".relatives");
+	char number[100];
+    sprintf(number, "%d", numgen);
+	strcat(filename,number);
+	strcat(filename,".txt");
     if ((file = fopen(filename, "w+")) == NULL) return 1; 
-    fprintf(file,"ID1 ID2 #of_relatives #closest_degree\n");
+    fprintf(file,"ID1.generation ID2.generation #of_relatives #closest_degree\n");
 	fflush(file);
     fclose (file);
 }
@@ -211,17 +244,52 @@ INT saverelative(char path[],INT indiv1,INT indiv2,INT nbrelative,INT lowestdegr
 	strcpy (filename,path);
 	strcat(filename,".relatives.txt");
     if ((file = fopen(filename, "a")) == NULL) return 1;
-    fprintf(file,"%d %d %d %d\n",indiv1+1-maxpeople[getgeneration(indiv1,maxpeople)],
+    fprintf(file,"%d %d %d %d %d\n",indiv1+1-maxpeople[getgeneration(indiv1,maxpeople)],
 				indiv2+1-maxpeople[getgeneration(indiv2,maxpeople)],
 				nbrelative,
 				lowestdegree);
+	fclose (file);
+}
+
+INT saveallrelative(char path[],INT indiv1,INT lastgeneration,struct structrelative *ptrelative,INT maxpeople[],INT numgen,INT numgenpeople)
+{	FILE * file;
+	char filename[100]; 
+	strcpy (filename,path);
+	strcat(filename,".relatives");
+	char number[100];
+    sprintf(number, "%d", numgen);
+	strcat(filename,number);
+	strcat(filename,".txt");
+    if ((file = fopen(filename, "a")) == NULL) return 1;
+    INT indiv2;
+	for(indiv2=1+indiv1;indiv2<maxpeople[lastgeneration+1] ;indiv2++) 
+	{	if (ptrelative->numrelative[indiv2]) fprintf(file,"%d.%d %d.%d %d %d\n",
+				indiv1+1-maxpeople[getgeneration(indiv1,maxpeople)],
+				getgeneration(indiv1,maxpeople),
+				indiv2+1-maxpeople[getgeneration(indiv2,maxpeople)],
+				getgeneration(indiv2,maxpeople),
+				ptrelative->numrelative[indiv2],
+				lastgeneration - ptrelative->highestgen[indiv2]);
+	}
+	if (numgenpeople>1)
+	{	for(indiv2=maxpeople[lastgeneration-1];indiv2<maxpeople[lastgeneration] ;indiv2++) 
+		{	if (ptrelative->numrelative[indiv2]) fprintf(file,"%d.%d %d.%d %d %d\n",
+				indiv1+1-maxpeople[getgeneration(indiv1,maxpeople)],
+				getgeneration(indiv1,maxpeople),
+				indiv2+1-maxpeople[getgeneration(indiv2,maxpeople)],
+				getgeneration(indiv2,maxpeople),
+				ptrelative->numrelative[indiv2],
+				lastgeneration - ptrelative->highestgen[indiv2]);
+		};	
+	};
 	fclose (file);
 }
 //
 //  main program
 //	
 int main(int argc, char *argv[])
-{	printf("Start...\n");
+{	 clock_t begin = clock();
+	printf("Start...\n");
 	//
 	printf("Reading arguments...\n");
 	//
@@ -245,11 +313,20 @@ int main(int argc, char *argv[])
 	printf("Last generation is %d\n",lastgeneration);
 	char path[100];
 	if (argc<7)
-	{   printf("The path of the run is missing. Default is .\n");
+	{   printf("The path of the run is missing. Default is /rc_scratch/emsa1620/GeneEvolve/Output_files300/.\n");
 	} else 
 	{	strcpy(path,argv[6]);
 	};
 	printf("The path is %s\n",path);
+	INT numgen;
+	if (argc<9)
+	{   printf("All pairs will be considered from $generation$ to $generation-numgen+1$. Default is $numgen=1$.\n");
+		numgen=1;
+	} else 
+	{	numgen = atoi (argv[8]);
+	};
+	
+	
 	//
 	// maxpeople store the number of people in previous generations, the following lines fill maypeople 
 	//
@@ -263,38 +340,73 @@ int main(int argc, char *argv[])
 	//
 	// people store for each eprson and each generation the ID of the mum and the ID of the dad, the following lines fill people 
 	//
-	INT degree=maxdegree;
+	INT degree=maxdegree+numgen-1;
 	generation=lastgeneration;
 	while (degree>0)
 	{	if (generation>0) readfile(path,generation,people,maxpeople);
 		degree=(degree>1)?degree-2:0;
 		generation=(generation>1)?generation-2:0;
 	};
+	if (numgen>1) readfile(path,lastgeneration-1,people,maxpeople);
 	//
 	// initsave creat the file relative.txt
 	//
-	initsave(path);
+	initsave(path,numgen);
 	//
 	// the following loop fill the file relative.txt by considering all pair of individual in the final generation
 	//
 	INT indiv1;
 	for(indiv1=maxpeople[lastgeneration];indiv1<maxpeople[lastgeneration+1];indiv1++)
-	{	printf("Compare ID %d and the rest of the world \n",indiv1+1-maxpeople[lastgeneration]);
+	{	printf("Compare ID %d.%d and the rest of the world \n",indiv1+1-maxpeople[lastgeneration],lastgeneration );
 		INT indiv2;
+		struct structrelative relative;
+		for(indiv2=0;indiv2<MAXPOP;indiv2++) 
+		{	relative.numrelative[indiv2]=0;
+			relative.highestgen[indiv2]=0;
+		};
+		struct structrelative *ptrelative=&relative;
 		for(indiv2=1+indiv1;indiv2<maxpeople[lastgeneration+1] ;indiv2++) 
-		{	struct structrelative relative;
-			relative.numrelative=0;
-			relative.highestgen=0;
-			struct structrelative *ptrelative=&relative;
-		//	printf("and ID %d:\n ",indiv2+1-maxpeople[lastgeneration]);
-			compare(indiv1,indiv2, lastgeneration-maxdegree,lastgeneration-1,ptrelative,maxpeople);
-			if (ptrelative->numrelative)
+		{	//	printf("and ID %d:\n ",indiv2+1-maxpeople[lastgeneration]);
+			compare(indiv1,indiv2,indiv2, lastgeneration-maxdegree,lastgeneration-1,ptrelative,maxpeople);
+			/*if (ptrelative->numrelative)
 			{//	printf("Print relative between ID %d and %d \n",indiv1+1-maxpeople[lastgeneration],indiv2+1-maxpeople[lastgeneration]);
 			//	printrelative(indiv1,indiv2,ptrelative,maxpeople,lastgeneration);
-				saverelative(path,indiv1,indiv2,ptrelative->numrelative,lastgeneration - ptrelative->highestgen,maxpeople);
-			};
+			//	saverelative(path,indiv1,indiv2,ptrelative->numrelative,lastgeneration - ptrelative->highestgen,maxpeople);
+			};*/
 		};
-		
+		if (numgen>1)
+		{	for(indiv2=maxpeople[lastgeneration-1];indiv2<maxpeople[lastgeneration] ;indiv2++) 
+			{	comparegenaway(indiv1,indiv2,indiv2, lastgeneration-maxdegree,lastgeneration-1,ptrelative,maxpeople);
+			};				
+		};
+		saveallrelative(path,indiv1,lastgeneration,ptrelative,maxpeople,numgen,numgen);
 	};
+	if (numgen>1)
+	{	lastgeneration--;
+		for(indiv1=maxpeople[lastgeneration];indiv1<maxpeople[lastgeneration+1];indiv1++)
+		{	printf("Compare ID %d.%d and the rest of the world \n",indiv1+1-maxpeople[lastgeneration],lastgeneration );
+			INT indiv2;
+			struct structrelative relative;
+			for(indiv2=0;indiv2<MAXPOP;indiv2++) 
+			{	relative.numrelative[indiv2]=0;
+				relative.highestgen[indiv2]=0;
+			};
+			struct structrelative *ptrelative=&relative;
+			for(indiv2=1+indiv1;indiv2<maxpeople[lastgeneration+1] ;indiv2++) 
+			{	//	printf("and ID %d:\n ",indiv2+1-maxpeople[lastgeneration]);
+				compare(indiv1,indiv2,indiv2,lastgeneration-maxdegree,lastgeneration-1,ptrelative,maxpeople);
+				/*if (ptrelative->numrelative)
+				{	printf("Print relative between ID %d and %d \n",indiv1+1-maxpeople[lastgeneration],indiv2+1-maxpeople[lastgeneration]);
+					printrelative(indiv1,indiv2,ptrelative,maxpeople,lastgeneration);
+				//	saverelative(path,indiv1,indiv2,ptrelative->numrelative,lastgeneration - ptrelative->highestgen,maxpeople);
+				};*/
+			};
+			saveallrelative(path,indiv1,lastgeneration,ptrelative,maxpeople,numgen,0);		
+		};
+	};
+			
+	clock_t end = clock();
+	float elapsed_secs = (float)(end - begin) ;
+    printf("\nTotal time:%f\n cpu click so %f seconds",elapsed_secs,elapsed_secs/CLOCKS_PER_SEC );
 	return (0);
-} 
+}
