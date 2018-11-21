3a73690d79612e1d86a0edaf47c9cd3b77497e5e
emmanuelsapin <emmanuelsapin@hotmail.com>
2018-11-21 15:07:19 -0700

    Update analyse.sh

--- analyse.sh
+++ analyse.sh
@@ -3,14 +3,12 @@
 #SBATCH --ntasks-per-node=10
 #SBATCH --nodes=1
 #SBATCH --qos=blanca-ibg
-#SBATCH --time=12:0:00
-#SBATCH --output=/rc_scratch/emsa1620/GeneEvolve/output.txt
+#SBATCH --time=48:0:00
 #SBATCH --error=/rc_scratch/emsa1620/GeneEvolve/error.txt
 
-echo $1
-
-echo $2
-
-echo $3
-
-./analyse --maxc-degree $degree --last-generation $generation --path $path
+ulimit -c 0 > /dev/null 2>&1
+ 
+/work/KellerLab/Emmanuel/GeneEvolve/analyse --maxc-degree $degree \
+											--last-generation $generation \
+											--path $path \
+											--num-generation $numgen
